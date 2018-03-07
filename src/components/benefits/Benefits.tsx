143e92dd8affcad7ca1bc7f6d02474669858b384
arne.hassel <arne.hassel@gmail.com>
2018-03-07 22:12:53 +0100

    Worked a bit on benefits

--- src/components/benefits/Benefits.tsx
+++ src/components/benefits/Benefits.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { TextData } from '../../api/texts';
-import MockupGroup from '../mockup_group/MockupGroup';
+
+// import MockupGroup from '../mockup_group/MockupGroup';
 
 class Benefits extends React.Component {
   public props: {
@@ -9,17 +10,17 @@ class Benefits extends React.Component {
 
   render() {
     return (
-      <div className="container">
-        <div className="benefits row">
-          <dl className="benefits__list col-lg-7 col-md-12 order-lg-2">
-            <h2 className="benefits__title title">Benefits</h2>
-            {this.props.text.value.map((item, key) => [
-              <dt key={key + 'dt'} className="benefits__list-item-label">{item.label}</dt>,
+      <div className="benefits">
+        <h2 className="benefits__title title">Benefits</h2>
+        <dl className="benefits__list">
+          {this.props.text.value.map((item, key) => (
+            <>
+              <dt key={key + 'dt'} className="benefits__list-item-label">{item.label}</dt>
               <dd key={key + 'dd'} className="benefits__list-item-value">{item.value}</dd>
-            ])}
-          </dl>
-          <MockupGroup />
-        </div>
+            </>
+          ))}
+        </dl>
+        {/*<MockupGroup/>*/}
       </div>
     );
   }
