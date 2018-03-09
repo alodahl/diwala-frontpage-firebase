25d16453427ea7c238fe3768822c87f71d32ef35
Laurynas Mickonis <mickonis.l@gmail.com>
2018-03-09 16:20:25 +0200

    changed header logo link

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,14 +1,13 @@
 import * as React from 'react';
 
 const logo = require('../../graphics/Diwala_Logo_258x108px_1.png');
-const indexUrl = window.location.host;
 
 export default function Header(props: any) {
   return (
     <header className="Header">
       <div className="container-fluid">
         <div className="row">
-          <a href={indexUrl}>
+          <a href="/">
             <img className="Header__logo" src={logo} alt="Diwala"/>
           </a>
         </div>
