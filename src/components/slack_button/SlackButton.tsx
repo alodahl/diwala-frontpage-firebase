3d19bf122561aadacb8f438c5332f8b253232fae
Snorre Lothar von Gohren Edwin <snorre.edwin@gmail.com>
2018-03-26 15:27:56 +0200

    renmae package. and change slack link

--- src/components/slack_button/SlackButton.tsx
+++ src/components/slack_button/SlackButton.tsx
@@ -4,7 +4,7 @@ import LinkButton from '../buttons/link/LinkButton';
 class SlackButton extends React.Component {
   public render() {
     const joinSlack = 'https://join.slack.com/t/';
-    const slackUrl = joinSlack + 'diwala-org/shared_invite/enQtMjIyODA4OTQ0MjEzLTZkMmU5MmNkNDg1YWEzNmM5Y2Q3NGYwYmMxMzkzMDJlMzBmZDdhOWUxNzNkZWJjNGEyZDhhYWY4NjA1ZDY2MTk';
+    const slackUrl = joinSlack + 'diwala-org/shared_invite/enQtMzM2OTQ0NDc2OTgyLTYwYjg4YjcwZjc3MmU2MWY0MjMzMzA4MWQ2NTEyN2ExMmFmZGYyNGEyMGFkMDVjNDkyMjM1YmZkMTViY2RlYTY';
     return (
       <LinkButton
         classes="button button--join-slack"
