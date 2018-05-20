68179bdcb1aabe2fbd6f8e7f69376eba0b9d27ed
Snorre Lothar von Gohren Edwin <snorre.edwin@gmail.com>
2018-05-20 22:29:40 +0200

    Made the hero object more flexible

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,12 +1,16 @@
 import * as React from 'react';
 import { TextData } from '../../api/texts';
 
-export default function Hero(props: { text: TextData }) {
+export default function Hero(props: { text: TextData, textId: String }) {
+  const tokenTextObj = props.text.value.find((text: any) => {
+    return text.label === props.textId;
+  });
+  const tokenText = tokenTextObj ? tokenTextObj.value : '';
   return (
     <div className="website-hero">
       <div className="website-hero__logo">
         <div className="website-hero__text">
-          {props.text.value[0].value}
+          {tokenText}
         </div>
       </div>
     </div>
