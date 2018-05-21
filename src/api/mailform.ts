7949dc08adf052df03962ed6ce9891c6b9f97035
Snorre Lothar von Gohren Edwin <snorre.edwin@gmail.com>
2018-05-21 19:04:47 +0200

    Added an method to target a different subscription list

--- src/api/mailform.ts
+++ src/api/mailform.ts
@@ -1,10 +1,22 @@
-import { post } from '../service/mailform';
+import { post, postSubscrption } from '../service/mailform';
 import { updateFormStatus } from '../actions/mailform';
 import { EXISTING_MEMBER, SIGNED_UP, UNKNOWN_ERROR, LOADING } from '../reducers/mailform';
 
-export default async function postPerson(dispatch: any, action: any, data: any) {
+export async function postPerson(dispatch: any, action: any, data: any) {
   dispatch(updateFormStatus(LOADING));
   const response = await post(dispatch, action, data);
+  if (response.ok) {
+    dispatch(updateFormStatus(SIGNED_UP));
+  } else if (!response.ok && response.message === EXISTING_MEMBER) {
+    dispatch(updateFormStatus(EXISTING_MEMBER));
+  } else {
+    dispatch(updateFormStatus(UNKNOWN_ERROR));
+  }
+}
+
+export async function postSubscriptionInterest(dispatch: any, action: any, data: any) {
+  dispatch(updateFormStatus(LOADING));
+  const response = await postSubscrption(dispatch, action, data);
   if (response.ok) {
     dispatch(updateFormStatus(SIGNED_UP));
   } else if (!response.ok && response.message === EXISTING_MEMBER) {
--- src/service/mailform.ts
+++ src/service/mailform.ts
@@ -4,30 +4,45 @@ interface FormPerson {
   email: string;
   firstname: string;
   lastname: string;
+  company?: string;
 }
+const getOptions = (url: string, data: FormPerson) => {
+  const company = data.company || '';
+  return {
+      method: 'POST',
+      uri: url,
+      headers: {
+        'User-Agent': 'Request-Promise',
+        'Content-Type': 'application/json;charset=utf-8'
+      },
+      json: true,
+      body: {
+        'email_address': data.email,
+        'firstname': data.firstname,
+        'lastname': data.lastname,
+        'company': company
+      }
+    };
+};
 
 export const post = async (dispatch: any, action: any, data: FormPerson)  => {
   const url = 'https://us-central1-diwala-frontpage-dev.cloudfunctions.net/mailChimSignup';
-  const options = {
-    method: 'POST',
-    uri: url,
-    headers: {
-      'User-Agent': 'Request-Promise',
-      'Content-Type': 'application/json;charset=utf-8'
-    },
-    json: true,
-    body: {
-      'email_address': data.email,
-      'firstname': data.firstname,
-      'lastname': data.lastname
-    }
-  };
-
+  const options = getOptions(url, data);
   try {
       const response = await rp(options);
       return response;
   } catch (e) {
       return e;
   }
+};
 
+export const postSubscrption = async (dispatch: any, action: any, data: FormPerson) => {
+  const url = 'https://us-central1-diwala-frontpage-dev.cloudfunctions.net/subscriptionSignup';
+  const options = getOptions(url, data);
+  try {
+      const response = await rp(options);
+      return response;
+  } catch (e) {
+      return e;
+  }
 };
