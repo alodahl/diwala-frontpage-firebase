27ac418cb122004ed4d3e7de7efb259a1aed6da7
Snorre Lothar von Gohren Edwin <snorre.edwin@gmail.com>
2018-05-22 00:45:53 +0200

    Added another token version and fixed up in typo.

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,14 +1,14 @@
 import * as React from 'react';
 import { TextData } from '../../api/texts';
 
-export default function Hero(props: { text: TextData, textId: String }) {
+export default function Hero(props: { text: TextData, textId: string, tokenVersion?: string }) {
   const tokenTextObj = props.text.value.find((text: any) => {
     return text.label === props.textId;
   });
   const tokenText = tokenTextObj ? tokenTextObj.value : '';
   return (
     <div className="website-hero">
-      <div className="website-hero__logo">
+      <div className={`website-hero__logo ${props.tokenVersion}`}>
         <div className="website-hero__text">
           {tokenText}
         </div>
