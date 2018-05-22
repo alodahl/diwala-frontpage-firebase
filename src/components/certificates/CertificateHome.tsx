4277729e2bbbf5560f18fc297e2a41c40adf01b1
Snorre Lothar von Gohren Edwin <snorre.edwin@gmail.com>
2018-05-22 08:42:12 +0200

    Adjusted offset to handle mobile and picture better

--- src/components/certificates/CertificateHome.tsx
+++ src/components/certificates/CertificateHome.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { TextData } from '../../api/texts';
 import Hero from '../hero/Hero';
 import ScrollToButton from '../buttons/scrollToButton/ScrollToButton';
+import { getViewportWidth } from '../../core/utilities/viewport.functions';
 
 class Home extends React.Component {
   public props: {
@@ -9,11 +10,14 @@ class Home extends React.Component {
   };
 
   public render() {
+    const screenWidth = getViewportWidth();
+    const offset = screenWidth < 1200 ? -100 : -500;
+    console.log(offset);
     return (
       <>
         <Hero text={this.props.text} textId="hero" tokenVersion="purple"/>
         <div className="website-hero__buttons">
-          <ScrollToButton id="#signup" text={this.props.text} offset={-500} textId="sign_up"/>
+          <ScrollToButton id="#signup" text={this.props.text} offset={offset} textId="sign_up"/>
           <ScrollToButton id="#footer" text={this.props.text} textId="question" invert/>
         </div>
       </>
