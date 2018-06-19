f386c2a0895d9f4f56fc5005c99de5c7f34a4dc4
Alina L <alodahl@pointloma.edu>
2018-06-19 16:45:45 -0700

    embed token mp4 to play on hover, keep backgrd png as fallback

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -6,8 +6,20 @@ export default function Hero(props: { text: TextData, textId: string, tokenVersi
     return text.label === props.textId;
   });
   const tokenText = tokenTextObj ? tokenTextObj.value : '';
+  const videoToken = require('../../graphics/diwala_token.mp4');
+  const v = document.getElementsByTagName('video')[0];
+  function playVideo() {
+      v.play();
+  }
+  function stopVideo() {
+      v.pause();
+  }
+
   return (
-    <div className="website-hero">
+    <div className="website-hero" onMouseOver={() => playVideo()} onMouseOut={() => stopVideo()}>
+      <video className="website-hero__video" loop preload="true">
+        <source src={videoToken} type="video/mp4" />
+      </video>
       <div className={`website-hero__logo ${props.tokenVersion}`}>
         <div className="website-hero__text">
           {tokenText}
