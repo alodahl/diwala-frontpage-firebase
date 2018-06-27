50e6247897925828dcdfcc29e08409d18c108d48
Sasha Bajzek <dabridginator@gmail.com>
2018-06-26 19:51:38 -0700

    Add news types

--- src/api/news.ts
+++ src/api/news.ts
@@ -1,5 +1,12 @@
 import pullNews from '../service/news';
+import { SanityImageData, SanityData } from './sanity';
 
 export default function getNews(dispatch: any, action: any) {
   pullNews(dispatch, action);
+}
+
+export interface NewsData extends SanityData {
+  source: string;
+  logo: SanityImageData;
+  url: string;
 }
