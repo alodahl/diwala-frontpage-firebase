dab951aa6bc3ba2821069f132d52d03f8b69bb8e
Alina L <alodahl@pointloma.edu>
2018-07-03 11:35:36 -0700

    fix url string in NewsItem

--- src/components/news_item/NewsItem.tsx
+++ src/components/news_item/NewsItem.tsx
@@ -1,23 +1,25 @@
 import * as React from 'react';
+import { urlFor } from '../../core/utilities/image-builder.functions';
 import { NewsData } from '../../api/news';
 
 export default function NewsItem(props: {news: NewsData[]}) {
 
   const newsItems = props.news.map((newsItem, key) => {
-   return (
-     <li key={newsItem._id} className="newsItem__list-item">
-         <div className="newsItem__section">
-           <a href={newsItem.url}>
-             <img className="newsItem__logo" src={newsItem.logo}/>
-           </a>
-         </div>
-         <div className="newsItem__section">
-           <a href={newsItem.url}>
-             <blockquote className="newsItem__quote">{newsItem.quote}</blockquote>
-           </a>
-         </div>
-     </li>
-    );
+    const url = urlFor(newsItem.logo).url();
+    return (
+       <li key={newsItem._id} className="newsItem__list-item">
+           <div className="newsItem__section">
+             <a href={newsItem.url}>
+               <img className="newsItem__logo" src={url}/>
+             </a>
+           </div>
+           <div className="newsItem__section">
+             <a href={newsItem.url}>
+               <blockquote className="newsItem__quote">{newsItem.quote}</blockquote>
+             </a>
+           </div>
+       </li>
+      );
   });
 
   return (
