2195779c2bb4df471c26580352fec76621bfd838
Sasha Bajzek <dabridginator@gmail.com>
2018-08-12 13:14:43 -0700

    Restructure team groupings

--- src/components/team/Team.tsx
+++ src/components/team/Team.tsx
@@ -6,19 +6,24 @@ const styles = require('../../compiled_css/components/team/Team.css');
 
 export default function Team(props: any) {
   const team: any[] = props.team;
-  const teamCore = team
-    .filter( teamMember => teamMember.type === 'core')
+
+  const teamTeam = team
+    .filter( teamMember => teamMember.type === 'core' || teamMember.type === 'tech-team' || teamMember.type === 'contributor')
     .sort((a: any, b: any) => a.order - b.order);
 
-  const teamOther = team
-    .filter( teamMember => teamMember.type !== 'core')
+  const teamAmbassador = team
+    .filter( teamMember => teamMember.type === 'ambassador')
+    .sort((a: any, b: any) => a.order - b.order);
+
+  const teamMentor = team
+    .filter( teamMember => teamMember.type === 'mentor')
     .sort((a: any, b: any) => a.order - b.order);
 
   const socialIconColor = styles.diwalaIconPurple;
 
-  const TeamCore = () => {
-    if (teamCore.length > 0) {
-      return teamCore.map((person: any, key: any) => {
+  const TeamTeam = () => {
+    if (teamTeam.length > 0) {
+      return teamTeam.map((person: any, key: any) => {
         const url = person.image ? urlFor(person.image).url() : '';
         return (
           <li key={person._id} className={`team__person team__person--role-${person.type}`}>
@@ -37,9 +42,30 @@ export default function Team(props: any) {
     }
   };
 
-  const TeamOther = () => {
-    if (teamOther.length > 0) {
-      return teamOther.map((person: any, key: any) => {
+  const TeamAmbassador = () => {
+    if (teamAmbassador.length > 0) {
+      return teamAmbassador.map((person: any, key: any) => {
+        const url = person.image ? urlFor(person.image).url() : '';
+        return (
+          <li key={person._id} className={`team__person team__person--role-${person.type}`}>
+            <img className="team__person-image" src={url}/>
+            <div className="team__person-name">{person.name}</div>
+            <div className="team__person-title" dangerouslySetInnerHTML={{__html: person.title}} />
+            <div className="team__person-social-icons">
+              <SocialIcons urls={person.linkedin ? [person.linkedin] : []} color={socialIconColor}/>
+              <SocialIcons urls={person.email ? [`mailto:${person.email}`] : []} color={socialIconColor}/>
+            </div>
+          </li>
+        );
+      });
+    } else {
+      return <span />;
+    }
+  };
+
+  const TeamMentor = () => {
+    if (teamMentor.length > 0) {
+      return teamMentor.map((person: any, key: any) => {
         const url = person.image ? urlFor(person.image).url() : '';
         return (
           <li key={person._id} className={`team__person team__person--role-${person.type}`}>
@@ -60,21 +86,28 @@ export default function Team(props: any) {
 
   return (
     <>
-      <div key="team-core" className="team team--core">
-        <div className="team__title-container team__title-container--core">
-          <h2 className="team__title team__title--core">Team</h2>
-          <h3 className="team__title team__title--others">Coworkers &amp; Mentors</h3>
+      <div key="team-team" className="team team">
+        <div className="team__title-container team__title-container">
+          <h3 className="team__title team__title--team">Team</h3>
         </div>
-        <ul className="team__list team__list-core">
-          {TeamCore()}
+        <ul className="team__list team__list">
+        {TeamTeam()}
         </ul>
       </div>
-      <div key="team-others" className="team team--others">
-        <div className="team__title-container team__title-container--others">
-          <h3 className="team__title team__title--others">Coworkers &amp; Mentors</h3>
+      <div key="team-ambassadors" className="team team">
+        <div className="team__title-container team__title-container">
+          <h3 className="team__title team__title--ambassadors">Ambassadors</h3>
         </div>
-        <ul className="team__list team__list--others">
-          {TeamOther()}
+        <ul className="team__list team__list">
+          {TeamAmbassador()}
+        </ul>
+      </div>
+      <div key="team-mentors" className="team team">
+        <div className="team__title-container team__title-container">
+          <h3 className="team__title team__title--mentors">Mentors &amp; Advisors</h3>
+        </div>
+        <ul className="team__list team__list">
+          {TeamMentor()}
         </ul>
       </div>
     </>
