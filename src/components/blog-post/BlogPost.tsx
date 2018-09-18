d6170756f52fa213e6779b9919b2a9ac9c978fab
Alina L <alodahl@pointloma.edu>
2018-09-18 10:19:43 -0700

    pass article data to BlogPost

--- src/components/blog-post/BlogPost.tsx
+++ src/components/blog-post/BlogPost.tsx
@@ -3,19 +3,39 @@ import * as BlockContent from '@sanity/block-content-to-react';
 // const BlockContent = require('@sanity/block-content-to-react');
 import { sanityClient } from '../../service/sanity';
 import { BlogData } from '../../api/blog';
+// import imageUrlBuilder from '@sanity/image-url';
 
 export default function BlogItem(props: BlogData) {
-  console.log(props);
+  console.log('Blog Data:', props);
+  // const builder = imageUrlBuilder(sanityClient);
+  // function urlFor(source: any) {
+  //   return builder.image(source);
+  // }
+  // for ( let i = 0; i < props.body.length; i++ ) {
+  //   if (props.body[i].type === 'image') {
+  //     props.body[i] = '<img src=' + urlFor(props.body[i]).url() + '/>';
+  //   }
+  // }
+  // const path = window.location.pathname;
+  //  const article = getArticle();
+  //  function getArticle(){
+  //    for(let i=0; i < BlogData.length; i++){
+  //      if(path === BlogData[i].path) {
+  //        return BlogData[i];
+  //      }
+  //    }
+  //  }
   return (
-  <div className="blog-post">
-    <h1 className="blog-item__title">{props.title}</h1>
-    <h2 className="blog-item__author">{props.author}</h2>
-    <BlockContent
-      blocks={props.body}
-      projectId={sanityClient.projectId}
-      dataset={sanityClient.dataset}
-    />
-  </div>
-  );
+    <div className="blog-post">
+      <h1 className="blog-item__title">{props.title}</h1>
+      <h2 className="blog-item__author">{props.author}</h2>
+      <h2 className="blog-item__date">{props.date}</h2>
+      <BlockContent
+        blocks={props.body}
+        projectId={sanityClient.projectId}
+        dataset={sanityClient.dataset}
+      />
+    </div>
+    );
 
 }
