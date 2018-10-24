66e1279308e9cc6c914a5d46598acd6988ba3a21
alodahl <alodahl@pointloma.edu>
2018-10-24 10:33:08 -0700

    start typing blog.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import Header from './components/header/Header';
-import Footer from './components/footer/Footer';
-import MainBox from './components/main_box/MainBox';
+// import Footer from './components/footer/Footer';
+// import MainBox from './components/main_box/MainBox';
 import NoMatch from './components/no_match/NoMatch';
 import Blog from './components/blog/Blog';
 import Certificates from './components/certificates/Certificates';
-import CuriousToLearnMore from './components/curious_to_learn_more/CuriousToLearnMore';
+// import CuriousToLearnMore from './components/curious_to_learn_more/CuriousToLearnMore';
 import Menu from './components/menu/Menu';
 import './compiled_css/App.css';
 import './compiled_css/fonts/font-faces.css';
@@ -27,17 +27,17 @@ const App = () => (
         <Menu />
         <Header />
         <Switch>
-          <Route exact={true} path="/" component={MainBox}/>
+          {/* <Route exact={true} path="/" component={MainBox}/> */}
           <Route exact={true} path="/certificates" component={Certificates}/>
           <Route exact={true} path="/blog" component={Blog}/>
           <Route component={NoMatch}/>
         </Switch>
-        <Footer>
+        {/* <Footer>
           <Switch>
             <Route exact={true} path="/" component={CuriousToLearnMore}/>
             <Route exact={true} path="/certificates" component={CuriousToLearnMore}/>
           </Switch>
-        </Footer>
+        </Footer> */}
       </div>
     </Router>
   </Provider>
