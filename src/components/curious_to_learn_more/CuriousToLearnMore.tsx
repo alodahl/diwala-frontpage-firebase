3f593dab3785d73d814b9e2033743989635a70ec
Sophie <sophie.helf@gmail.com>
2018-11-20 08:57:38 -0800

    changed .org to .io

--- src/components/curious_to_learn_more/CuriousToLearnMore.tsx
+++ src/components/curious_to_learn_more/CuriousToLearnMore.tsx
@@ -12,7 +12,7 @@ export default function CuriousToLearnMore(props: any) {
         <div className="Curious__buttons flex-btn-group">
           <LinkButton
             classes="button invert-button"
-            url="mailto:hello@diwala.org"
+            url="mailto:hello@diwala.com"
             text="Send us an email"
           />
           <LinkButton
