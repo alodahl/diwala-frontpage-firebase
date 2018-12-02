8aff85224c44a0330dbeeb1e151c4c064e0c8208
Snorre lothar von Gohren Edwin <snorre.edwin@gmail.com>
2018-12-02 21:05:17 +0300

    rewamed the button to download the app from the right shop

--- src/components/landing_student/LandingContentStudent.tsx
+++ src/components/landing_student/LandingContentStudent.tsx
@@ -6,6 +6,15 @@ const mobileImage = require('../../graphics/landing_phone.png');
 const appStoreImage = require('../../graphics/app_store.png');
 const googlePlayImage = require('../../graphics/google_play.png');
 
+const android = 'Android';
+const ios = 'iOS';
+
+const getOS = () => {
+  const UAParser = (window as any).UAParser;
+  const parser = new UAParser();
+  return parser.getResult();
+};
+
 export default function LandingContent(props: {
   text: TextData[];
   id: string;
@@ -24,6 +33,51 @@ export default function LandingContent(props: {
   }
   renderDataObj = createRenderDataObj(filteredData);
 
+  const data = getOS();
+  const os = data.os.name;
+  let downloadUrl;
+  if (os === android) {
+    downloadUrl = renderDataObj.googlePlayStoreLink;
+  }
+  if (os === ios) {
+    downloadUrl = renderDataObj.appleAppStoreLink;
+  }
+
+  const getFallbackComp = () => {
+    return (
+      <div className="row app-button-group custom-button-group">
+        <div className="google-play-btn-container">
+          <a target="_blank" href={renderDataObj.googlePlayStoreLink}>
+            <img className="google-play" src={googlePlayImage} />
+          </a>
+        </div>
+        <div className="app-store-btn-container">
+          <a target="_blank" href={renderDataObj.appleAppStoreLink}>
+            <img className="app-store" src={appStoreImage} />
+          </a>
+        </div>
+      </div> 
+    );
+  };
+
+  const downloadButton = () => {
+    return (
+      <div className="join-now-btn-container">
+        <a className="join-now-btn" target="_blank" href={downloadUrl}>
+          Download
+        </a>
+      </div>
+    );
+  };
+
+  const getComponent = () => {
+    if (os === ios || os === android) {
+      return downloadButton();
+    } else {
+      return getFallbackComp();
+    }
+  };
+
   return (
     <div className="landing">
       <img className="token-bg" src={tokenImage} />
@@ -38,18 +92,7 @@ export default function LandingContent(props: {
             <div className="landing-description">
               <p>{renderDataObj.description}</p>
             </div>
-            <div className="row app-button-group custom-button-group">
-              <div className="google-play-btn-container">
-                <a target="_blank" href={renderDataObj.googlePlayStoreLink}>
-                  <img className="google-play" src={googlePlayImage} />
-                </a>
-              </div>
-              <div className="app-store-btn-container">
-                <a target="_blank" href={renderDataObj.appleAppStoreLink}>
-                  <img className="app-store" src={appStoreImage} />
-                </a>
-              </div>
-            </div> 
+            {getComponent()}
           </div>
           <div className="col-sm-6 col-md-6 landing-secondary-container">
             <img className="mobile" src={mobileImage} />
